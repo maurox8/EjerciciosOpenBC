88b9b0f101e5ed05c1d36cd835b74524e8609305
maurox8 <maurox93@gmail.com>
2023-06-10 17:36:39 +0200

    Update Ej6p1OBCPython.py

--- Ej6p1OBCPython.py
+++ Ej6p1OBCPython.py
@@ -1,16 +1,20 @@
 class Vehiculo:
-    Color = None
-    Ruedas = None
-    Puertas = None
-
+    def __init__(self, Color, Ruedas, Puertas):
+        self.Color = Color
+        self.Ruedas = Ruedas
+        self.Puertas = Puertas
+    
 class Coche(Vehiculo):
-    Velocidad = None
-    Cilindrada = None
+    def __init__(self, Color, Ruedas, Puertas, Velocidad, Cilindrada):
+        self.Color = Color
+        self.Ruedas = Ruedas
+        self.Puertas = Puertas
+        self.Velocidad = Velocidad
+        self.Cilindrada = Cilindrada
+    def __str__(self):
+        return "coche de color {}, que alcanza los {} kmph, con {} ruedas, {} puertas y {} cc de cilindrada".format(self.Color, self.Velocidad, self.Ruedas, self.Puertas, self.Cilindrada)
 
-v = Vehiculo()
-c = Coche()
-
-c.Color= 'Blanco'
-c.Velocidad = 180
-
-print('El coche es de color', c.Color, 'y alcanza los', c.Velocidad, 'kmph')
+coche1 = Coche('Blanco', 4, 5, 160, 200)
+coche2 = Coche('negro', 4, 5, 180, 250)
+print(coche1)
+print(coche2)
